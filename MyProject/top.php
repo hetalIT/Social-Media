102492a4b7d75ef7dbacf938824172911d856ddf
nehaparneriya <35649475+nehaparneriya@users.noreply.github.com>
2018-09-25 22:07:18 -0700

    Add files via upload

---
+++ MyProject/top.php
@@ -0,0 +1,40 @@
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+     
+       <link rel="stylesheet" type="text/css" href="css/bootst.css">
+</head>
+<body>
+   <div id=head>
+    <div class="top">
+        <div class="topl">
+             <img src="img/logo.jpg" height="40px" width="40px">
+             <img src="img/profile.png" height="40px" width="40px" style="border-radius:50%;background-color:pink">
+             <p>Neha Parneriya</p>
+        </div>
+        <div class="topr">
+         <ul>
+             <li>Home</li>
+             <li>About us</li>
+             <li>Logout</li>
+             <li>Help</li>    
+         </ul>
+         <input id="search" type="text"  style="margin:10px 0px 0px 170px;width:30%;height:30px;border-radius:50px 50px 50px 50px;float:left" size="45%"/><img src="img/sr.png" height="17px" width="17px" style="margin-left:-20px;margin-top:16px;float:left">
+         <div class="downmenu">
+             <img src="img/down.png" height="20px" width="20px" style="margin-top:15px;margin-left:30px;float:left"/> 
+                  <div class="popup">
+                    <ul>
+                        <li>Settings</li>
+                        <li>Logout</li>
+                    </ul>
+                </div>
+         </div>
+        </div>
+        
+    </div>
+    </div>
+</body>
+</html>
